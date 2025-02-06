50666b8eca7e47373e1364e3311b8af9148d8f9e
Pankaj Teron <pnkj123teron@gmail.com>
2025-02-07 02:30:29 +0530

    commit

--- components/header.jsx
+++ components/header.jsx
@@ -8,13 +8,13 @@ import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigge
 const Header = () => {
   return (
     <header className='fixed top-0 w-full border-b bg-backgroud/80 backdrop-blur-md z-50 supports-[backdrop-filter]:bg-background/60'>
-      <nav className='container mx-auto px-4 h-16 flex items-center justify-between'>
+      <nav className='container mx-auto px-4 h-15 flex items-center justify-between'>
         <Link href="/">
           <Image src="/logo.png" 
           alt="Sensai Logo" 
           width={200}
           height={60}
-          className="h-12 py-1 w-auto object-contain" 
+          className="h-11 py-1 w-auto object-contain" 
           />
         </Link>
 
